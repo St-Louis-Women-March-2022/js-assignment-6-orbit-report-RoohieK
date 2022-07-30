d1628ccd701aef7ec111fad7a7054defcc837af5
roohiek <roohiekaushik@gmail.com>
2022-07-30 06:42:49 +0530

    Commit after addition satellite count bonus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,11 +41,10 @@ export class AppComponent {
 		let matchingSatellites: Satellite[] = [];
 		searchTerm = searchTerm.toLowerCase();
 		for(let i=0; i < this.sourceList.length; i++) {
-			//let name = this.sourceList[i].name.toLowerCase();
+			let name = this.sourceList[i].name.toLowerCase();
 			let orbitType = this.sourceList[i].orbitType.toLowerCase();
 			let type = this.sourceList[i].type.toLowerCase();
-			//if (name.indexOf(searchTerm) >= 0) {
-				if(orbitType.indexOf(searchTerm )>=0 || type.indexOf(searchTerm ) >=0){
+			if(orbitType.indexOf(searchTerm )>=0 && type.indexOf(searchTerm ) >=0 || name.indexOf(searchTerm) >= 0){
 				matchingSatellites.push(this.sourceList[i]);
 			}
 		}
